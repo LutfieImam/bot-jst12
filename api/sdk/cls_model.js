1086ec6e7578cf2dba48d1d7576f41450ea86be2
LutfieImam <84964463+LutfieImam@users.noreply.github.com>
2021-06-05 19:07:57 +0700

    Update cls_model.js

--- api/sdk/cls_model.js
+++ api/sdk/cls_model.js
@@ -30,7 +30,7 @@ async function classify(data){
 
     try{
         // path load in public access => github
-        const path = 'https://raw.githubusercontent.com/zendi014/jst_service/main/public/cls_model/model.json';
+        const path = 'https://raw.githubusercontent.com/LutfieImam/bot-jst12/main/public/cls_model/model.json';
         const model = await tf.loadGraphModel(path);
         
         predict = model.predict(
